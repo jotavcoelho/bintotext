32c8c0d663515b47a08244cba69272fe222cdf91
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2020-08-26 10:14:59 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Text } from 'react-native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Text>Hello world</Text>
+    </NavigationContainer>
+  )
+}
