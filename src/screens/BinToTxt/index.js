4c8f02096f2053de304cdf79ca8aa31fffdd67b2
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2020-08-27 07:46:01 -0300

    Styled Binary Keyboard (Not functional yet)

--- src/screens/BinToTxt/index.js
+++ src/screens/BinToTxt/index.js
@@ -1,28 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Keyboard, Pressable } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { Container, OnlyText, Input, ConvertButton, ButtonContainer, ButtonText } from './styles';
+import { 
+  Container, 
+  OnlyText, 
+  Input, 
+  ConvertButton, 
+  ButtonPressContainer, 
+  BinKeyboard,
+  DigitsContainer,
+  Digit,
+  OneOrZero,
+  FuncButtonsContainer,
+  Spacebar,
+  Backspace,
+} from './styles';
 
 const BinToTxt = () => {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+
+  const [visibleBinKeyb, setVisibleBinKeyb] = useState(false);
+  
+  function buttonPress() {
+    setVisibleBinKeyb(false);
+  }
+
   return (
     <Container>
-      <OnlyText>Binary:</OnlyText>
-      <Input 
-        multiline={true}
-        textAlignVertical="top"
-      />
-      <ButtonContainer>
-        <ConvertButton>
-          <OnlyText>CONVERT</OnlyText>
-        </ConvertButton>
-      </ButtonContainer>
+        <OnlyText>Binary:</OnlyText>
+      <Pressable
+        onPress={() => setVisibleBinKeyb(true)}
+      >
+        <Input 
+          multiline={true}
+          textAlignVertical="top"
+          editable={false}
+          value={input}
+        />
+      </Pressable>
+        <ButtonPressContainer
+          onPress={() => setVisibleBinKeyb(false)}
+        >
+          <ConvertButton>
+            <OnlyText>CONVERT</OnlyText>
+          </ConvertButton>
+        </ButtonPressContainer>
       <OnlyText>Resulting Text:</OnlyText>
       <Input 
         multiline={true}
         textAlignVertical="top"
         editable={false}
         placeholder="The decoded text will appear here"
+        value={output}
       />
+      <BinKeyboard visible={visibleBinKeyb} >
+        <DigitsContainer>
+          <Digit>
+            <OneOrZero>1</OneOrZero>
+          </Digit>
+          <Digit>
+            <OneOrZero>0</OneOrZero>
+          </Digit>
+        </DigitsContainer>
+        <FuncButtonsContainer>
+          <Spacebar>
+            <Icon name="keyboard-space" color="#000" size={20} />
+          </Spacebar>
+          <Backspace>
+            <Icon name="keyboard-backspace" color="#000" size={20} />
+          </Backspace>
+        </FuncButtonsContainer>
+      </BinKeyboard>
     </Container>
   );
 }
--- src/screens/BinToTxt/styles.js
+++ src/screens/BinToTxt/styles.js
@@ -1,8 +1,10 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
-import { RectButton } from 'react-native-gesture-handler';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Pressable } from 'react-native';
 
 export const Container = styled.View`
+  position: relative;
   flex: 1;
   background-color: #efefef
   flex-direction: column;
@@ -17,7 +19,9 @@ export const OnlyText = styled.Text`
 
 export const Input = styled.TextInput.attrs({
   selectionColor: "#444",
+  placeholderTextColor: "#666"
 })`
+  color: #333;
   height: 180px;
   background-color: #FFF;
   border-radius: 6px;
@@ -26,15 +30,14 @@ export const Input = styled.TextInput.attrs({
   border: 1px solid #ccc;
 `;
 
-export const ButtonContainer = styled.View`
-  flex: 1;
+export const ButtonPressContainer = styled(Pressable)`
+  height: 60px;
   border: 3px solid #111;
   border-radius: 6px;
   margin: 10px;
-
 `;
 
-export const ConvertButton = styled(RectButton)`
+export const ConvertButton = styled(BorderlessButton)`
   flex: 1;
   align-items: center;
   justify-content: center;
@@ -42,6 +45,70 @@ export const ConvertButton = styled(RectButton)`
   border-radius: 6px;
 `;
 
-export const ButtonText = styled.Text`
+export const BinKeyboard = styled.View`
+  position: absolute;
+  height: 250px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: #fff;
+  flex-direction: column;
+  border: 2px solid #0c0c0c;
+  border-radius: 6px;
 
+  opacity: 0;
+  display: none;
+
+  ${props => props.visible && css`
+    opacity: 1;
+    display: flex;
+  `}
+  
+  padding: 10px 5px;
+
+`;
+
+export const DigitsContainer = styled.View`
+  flex-direction: row;
+  flex: 1;
+`;
+
+export const Digit = styled(Pressable)`
+  flex: 0.5;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  margin: 15px 10px;
+  border: 3px solid #0c0c0c;
+  border-radius: 6px;
+`;
+
+export const OneOrZero = styled.Text`
+  font-size: 40px;
+  font-weight: bold;
+`;
+
+export const FuncButtonsContainer = styled.View`
+  flex-direction: row;
+  flex: 0.2;
+`;
+
+export const Spacebar = styled(Pressable)`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  margin: 0 3px;
+  border: 3px solid #0c0c0c;
+  border-radius: 6px;
+`;
+
+export const Backspace = styled(Pressable)`
+  flex: 0.13;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  margin: 0 3px;
+  border: 3px solid #0c0c0c;
+  border-radius: 6px;
 `;
