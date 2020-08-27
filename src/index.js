a61796311fc69e166ef2f20fc984cad57cb3bb94
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2020-08-27 06:26:05 -0300

    Styled StatusBar

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { Text } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import Nav from './nav';
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar 
+        backgroundColor="#efefef"
+        barStyle="dark-content"
+      />
       <Nav />
     </NavigationContainer>
   )
